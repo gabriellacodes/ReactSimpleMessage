efe9359fc4318a2b990505c5095ceaf6e6de8d05
peidcud <51078365+peidcud@users.noreply.github.com>
2020-09-12 10:29:46 +0800

    Completed exercise

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = React.useState("JavaScript is so cool.");
+  
+  function handleClick() {setMessage("my updated message");
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>{message}</h1>
+    <button onClick={handleClick}>Update the message!</button>
     </div>
   );
 }
